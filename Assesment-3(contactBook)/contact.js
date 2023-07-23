279ad52c465c9ae4383901fe8e317d8a758d2595
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-07-23 23:17:16 +0300

    updated contacts app

--- Assesment-3(contactBook)/contact.js
+++ Assesment-3(contactBook)/contact.js
@@ -1,21 +1,51 @@
 
-const addButton = document.getElementById('button');
+const addButton = document.getElementById('addbtn');
 const contactCardContainer = document.querySelector('.contactCard');
+
+addButton.addEventListener('click', () => {
+  console.log('clicked');
+window.location.href = 'newContact.html';
+});
 
 
 function displayContacts() {
   contactCardContainer.innerHTML = '';
 
-  const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
-console.log('disp');
+  const contacts = JSON.parse(localStorage.getItem('contacts')) ;
+  contacts.forEach((contact, index) => {
+    console.log(`Contact: ${contact.name} Phone: ${contact.phone}`);
+  });
+// console.log(contacts);
   contacts.forEach((contact, index) => {
     const container=document.createElement('div');
     container.classList.add('user');
+    
 
-    const contactCards=document.createElement(contact,index);
-    contactCardContainer.appendChild(contactCards);
+
+    const phoneName =document.createElement('div');
+    phoneName.classList.add('name');
+    phoneName.textContent=contact.name;
+    container.appendChild(phoneName);
+
+    const phoneNo =document.createElement('div');
+    phoneNo.classList.add('number');
+    phoneNo.textContent=contact.phone;
+container.appendChild(phoneNo);
+
+const deleteBtn=document.createElement('button');
+deleteBtn.textContent="x";
+deleteBtn.style.height="30px";
+container.appendChild(deleteBtn);
+
+deleteBtn.addEventListener('click',()=>{
+deleteContact(index);
+})
+
+    // const contactCards=document.createElement('div');
+    contactCardContainer.appendChild(container);
     
     // container.appendChild(contactCardContainer);
+    // console.log('end');
   });
 }
 
@@ -37,15 +67,17 @@ function updateContact(index, name, phone) {
   }
 }
 
-function createContactCard(contact, index) {
-
+function deleteContact(index) {
+  const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+  if (index >= 0 && index < contacts.length) {
+    contacts.splice(index, 1);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    displayContacts(); 
+  }
 }
 
 
-addButton.addEventListener('click', () => {
-  
-  window.location.href = 'newContact.html';
-});
+
 displayContacts();
 
 
--- Assesment-3(contactBook)/new.js
+++ Assesment-3(contactBook)/new.js
@@ -1,27 +1,29 @@
 
-const nameInput = document.getElementById('name');
-const phoneInput = document.getElementById('tel');
-const saveButton = document.getElementById('save');
+const nameInput = document.querySelector('.name');
+const phoneInput = document.querySelector('.tel');
+const saveButton = document.querySelector('.savebtn');
+
+
+
 
 function saveContact(name, phone) {
-  const contact = { name, phone };
-  const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
-  contacts.push(contact);
-  localStorage.setItem('contacts', JSON.stringify(contacts));
-}
+    const contact = { name, phone };
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    contacts.push(contact);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  };
 
-
-saveButton.addEventListener('click', () => {
+  saveButton.addEventListener('click', function()  {
     console.log("click");
-  const name = nameInput.value;
-  const phone = phoneInput.value;
+  var name = nameInput.value;
+  var phone = phoneInput.value;
 
 
   saveContact(name, phone);
 
  
-  nameInput.value = '';
-  phoneInput.value = '';
+  // nameInput.value = '';
+  // phoneInput.value = '';
 
   window.location = 'contact.html';
 });
