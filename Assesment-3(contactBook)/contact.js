64fd6f436e22a90d573abac8f305e9d3ba6a6bf0
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-07-24 11:15:26 +0300

    updated the styles for contact book landing page

--- Assesment-3(contactBook)/contact.js
+++ Assesment-3(contactBook)/contact.js
@@ -17,32 +17,48 @@ function displayContacts() {
   });
 // console.log(contacts);
   contacts.forEach((contact, index) => {
+const totalContainer=document.createElement('div');
+totalContainer.classList.add('mainContainer');
+
     const container=document.createElement('div');
     container.classList.add('user');
-    
 
-
+    const lineBreak=document.createElement('div');
+    lineBreak.classList.add('breaker');
+    const line=document.createElement('hr');
+    lineBreak.appendChild(line);
+  
     const phoneName =document.createElement('div');
     phoneName.classList.add('name');
     phoneName.textContent=contact.name;
     container.appendChild(phoneName);
 
+
     const phoneNo =document.createElement('div');
     phoneNo.classList.add('number');
     phoneNo.textContent=contact.phone;
-container.appendChild(phoneNo);
+    container.appendChild(phoneNo);
 
 const deleteBtn=document.createElement('button');
 deleteBtn.textContent="x";
 deleteBtn.style.height="30px";
+deleteBtn.style.width="50px";
+deleteBtn.style.backgroundColor="#905554"
 container.appendChild(deleteBtn);
+
+lineBreak.appendChild(line);
+// container.appendChild(lineBreak);
+
+totalContainer.appendChild(container);
+totalContainer.appendChild(lineBreak);
 
 deleteBtn.addEventListener('click',()=>{
 deleteContact(index);
 })
 
     // const contactCards=document.createElement('div');
-    contactCardContainer.appendChild(container);
+    contactCardContainer.appendChild(totalContainer);
+    
     
     // container.appendChild(contactCardContainer);
     // console.log('end');
